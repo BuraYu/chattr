ffcd6792ac68e2091037086d664bb6591f456b74
BuraYu <burak.yueksel@hotmail.com>
2025-06-04 22:47:06 +0200

    feat: add ChatAre and ChatHeader components

---
+++ src/components/chat/ChatArea.tsx
@@ -0,0 +1,9 @@
+import ChatHeader from "./ChatHeader";
+
+export default function ChatArea() {
+  return (
+    <div className="">
+      <ChatHeader />
+    </div>
+  );
+}
---
+++ src/components/chat/ChatHeader.tsx
@@ -0,0 +1,15 @@
+import { Hash, Ellipsis } from "lucide-react";
+
+export default function ChatHeader() {
+  return (
+    <div className="flex justify-between border-b-2 p-4">
+      <div className="flex items-center gap-1">
+        <Hash className="w-4 h-4" />
+        <h2 className="text-base leading-none font-bold">general</h2>
+      </div>
+      <button className="p-2 rounded-full hover:bg-gray-200 transition">
+        <Ellipsis w-3 />
+      </button>
+    </div>
+  );
+}
