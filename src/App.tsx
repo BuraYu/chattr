662cfcc68e76385efe61251fe495b478a5696867
BuraYu <burak.yueksel@hotmail.com>
2025-06-08 15:57:05 +0200

    add: supabase to project and tested it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import AppLayout from "./components/layout/AppLayout";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+interface Instrument {
+  id: string;
+  name: string;
+}
 
 function App() {
-  return <AppLayout />;
+  const [instruments, setInstruments] = useState<Instrument[]>([]);
+
+  useEffect(() => {
+    async function getInstruments() {
+      const { data, error } = await supabase.from("instruments").select("*");
+
+      if (error) {
+        return;
+      }
+
+      if (data) {
+        setInstruments(data);
+      }
+    }
+
+    getInstruments();
+  }, []);
+
+  return (
+    <ul>
+      {instruments.map((instrument) => (
+        <li key={instrument.id}>{instrument.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
